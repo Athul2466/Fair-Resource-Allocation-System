70bc47731d89ec08dc3df3dc70d26a0bf39e414c
Athul2466 <athul.k2466@gmail.com>
2026-04-20 22:43:34 +0530

    Fourth commit

---
+++ backend.js
@@ -0,0 +1,9 @@
+let processes = [];
+let processId = 1;
+
+const addProcess=()=>{
+    const name = document.getElementById("pname").value;
+    const arrival = parseInt(document.getElementById("arrival").value);
+    const burst = parseInt(document.getElementById("burst").value);
+    const priority = parseInt(document.getElementById("priority").value);
+}
