1946fff683cb1b0e4a24d1f839dc2081e42684ea
Athul2466 <athul.k2466@gmail.com>
2026-04-21 10:20:25 +0530

    Fifth Commit

--- backend.js
+++ backend.js
@@ -6,4 +6,14 @@ const addProcess=()=>{
     const arrival = parseInt(document.getElementById("arrival").value);
     const burst = parseInt(document.getElementById("burst").value);
     const priority = parseInt(document.getElementById("priority").value);
+
+    console.log("",name)
+    console.log("",arrival)
+     console.log("",burst)
+    console.log("",priority)
+
+     if (!/^[a-zA-Z0-9]+$/.test(name)) {
+        alert("Process Name should contain only letters and numbers.");
+        return;
+     }
 }
